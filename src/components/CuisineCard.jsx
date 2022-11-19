f5c539030c16cfd4ac45e13ec458465aad4a7382
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-11-19 21:17:35 +0100

    --

--- src/components/CuisineCard.jsx
+++ src/components/CuisineCard.jsx
@@ -1,19 +1,26 @@
 import { Link } from "react-router-dom";
+import { IoTimerOutline } from "react-icons/io5";
 
 
 export default function CuisineCard({recipe}) {
 
     return (
         <>
-        <article className="cuisine-card" key={recipe.id}>
-            <Link to={'/recipe/'+ recipe.id}>
+         <Link to={'/recipe/'+ recipe.id} key={recipe.id}>
+            <article className="cuisine-card">
                 <div className="cuisine-card__img-cntr">
                     <img src={recipe.image} alt="" />
                 </div>
                 
                 <h4>{recipe.title}</h4>
-            </Link>
-        </article>
+                <div className="cuisine-card__text">
+                    <p><IoTimerOutline/> {recipe.readyInMinutes} min</p>
+                    <p>Servings: {recipe.servings}</p>
+                    <p>{recipe.diets}</p>
+                    <p>{recipe.summary}</p>
+                </div>   
+            </article>
+        </Link>
         </>
     )
 }
