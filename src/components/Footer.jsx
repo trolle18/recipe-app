4edca74163ef2ab5e3a340c9d32ba7a1dcaae94e
Sofie Trolle <92292475+trolle18@users.noreply.github.com>
2022-12-29 21:32:57 +0100

    --

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,47 @@
 import { Link, NavLink } from "react-router-dom";
-import Categories from "./Categories";
-import { GiKnifeFork } from "react-icons/gi";
+import { GiKnifeFork, GiNoodles, GiSushis } from "react-icons/gi";
 import { IoHeart } from "react-icons/io5";
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
-import { GiNoodles, GiSushis } from 'react-icons/gi';
 
 
 export default function Footer() {
     return (
         <>
         <footer>
-            <section className="">
-                <Link to={"/"} className="">
-                    <h1><GiKnifeFork/> Recipes</h1>                   
+            <section className="footer-section">
+                <Link to={"/"} className="category-link main-link">
+                    <GiKnifeFork/>
+                    <h1>Recipes</h1>                   
                 </Link>
             </section>  
 
-            <section  className="">
-                <Link to={'/cuisine/italian'} className="category-link">
-                    <h4><FaPizzaSlice/> Italian</h4>
-                </Link>
+            <section  className="footer-section">
+                <NavLink to={'/cuisine/italian'} className="category-link">
+                   <FaPizzaSlice/>
+                   <h4> Italian</h4>
+                </NavLink>
 
-                <Link to={'/cuisine/american'} className="category-link">
-                    <h4><FaHamburger/> American</h4>
-                </Link>
+                <NavLink to={'/cuisine/american'} className="category-link">
+                    <FaHamburger/>
+                    <h4>American</h4>
+                </NavLink>
 
-                <Link to={'/cuisine/thai'} className="category-link">
-                    <h4><GiNoodles/> Thai</h4>
-                </Link>
+                <NavLink to={'/cuisine/thai'} className="category-link">
+                    <GiNoodles/>
+                    <h4>Thai</h4>
+                </NavLink>
 
-                <Link to={'/cuisine/japanese'} className="category-link">
-                    <h4><GiSushis/> Japanese</h4>
-                </Link>
-
-                <Link to={'/favorites'} className="category-link">
-                    <h4><IoHeart/> Favorites</h4>
-                </Link>
-               
-            </section>    
+                <NavLink to={'/cuisine/japanese'} className="category-link">
+                   <GiSushis/>
+                   <h4>Japanese</h4>
+                </NavLink>           
+            </section>
+            <section  className="footer-section">
+                <NavLink to={'/favorites'} className="category-link">
+                    <IoHeart/> 
+                    <h4>Favorites</h4>
+                </NavLink>               
+            </section>
         </footer>
            
         </>
